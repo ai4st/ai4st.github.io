daf9f2e90bd726922831b4db1451d0cd59843a05
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2022-11-27 14:47:23 +0100

    use svg icon instead of lineicons cdn

--- js/tracks.js
+++ js/tracks.js
@@ -5,7 +5,6 @@ for (let section_index = 0; section_index < titles.length; section_index++) {
   const title = titles[section_index];
   title.addEventListener('click', () => {
     contents[section_index].classList.toggle('hide');
-    title.childNodes[1].classList.toggle('lni-angle-double-down');
-    title.childNodes[1].classList.toggle('lni-angle-double-up');
+    title.childNodes[1].classList.toggle('flip');
   });
 }
